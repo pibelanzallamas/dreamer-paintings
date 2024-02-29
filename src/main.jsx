de016a70fb0d3eef2fc09e698f48b142c4bde4d3
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-02-29 14:20:41 -0300

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./state/store";
 import { ReactNotifications } from "react-notifications-component";
@@ -9,11 +9,11 @@ import "react-notifications-component/dist/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <ReactNotifications />
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
