711620b1f95e7e6986ac1ddb269b1875cffbd219
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-05-24 19:19:01 -0300

    vercel update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import AppRoutes from "./components/AppRoutes";
 import "./styles/App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div>
+      <Analytics />
       <Navbar />
       <AppRoutes />
     </div>
